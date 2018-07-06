87db7f453a89e837a3688381cd52e9682f25cfb5
xlsn0w <xlsn0wios@gmail.com>
2018-07-06 14:19:59 +0800

    add

---
+++ Swift-4.2/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Swift-4.2
+//
+//  Created by HL on 2018/7/6.
+//  Copyright © 2018年 XL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var array = [String]();
+    var array2 = Array<String>()
+    
+    enum XLType : Int {
+        case XLType_default
+        case XLType_custom
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var array = [Int]()
+        array.append(1)
+        print(array)
+        array.append(88)
+        print("\(array)")//\()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
