a703b9ef4fd3b921dce33768374031cd9b012876
xlsn0w <xlsn0wios@gmail.com>
2018-08-17 11:25:45 +0800

    add

---
+++ Swift-4.2/NSKeyedUnarchiverViewController.swift
@@ -0,0 +1,70 @@
+//
+//  NSKeyedUnarchiverViewController.swift
+//  Swift-4.2
+//
+//  Created by HL on 2018/8/17.
+//  Copyright © 2018年 XL. All rights reserved.
+//
+
+import UIKit
+
+class NSKeyedUnarchiverViewController: UIViewController {
+
+    // 3.0 实现归档把模型保存到本地Document文件夹:
+    // 3.1 获取本地Document路径,一般路径都设为全局变量,方便解档直接使用:
+    let userAccountPath = "\(NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask,true).first!)/userAccount.data"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let userModel = Content()
+        userModel.id = 419
+        userModel.nickname = "小韩哥"
+        userModel.phone = "1588888888"
+        
+        // 归档
+        saveWriteNSKeyedArchiver(userModel: userModel)
+        
+        // 解档
+        readWithNSKeyedUnarchiver { (model) in
+            print(model.nickname!)
+        }
+    }
+    
+    // MARK - 归档
+    func saveWriteNSKeyedArchiver(userModel : Content) {
+        
+        // 3.2 对获取到的模型进行归档操作,要注意模型必须是确定的类型,如果是可选类型会报发送未识别的消息的错误(切记)
+        NSKeyedArchiver.archiveRootObject(userModel, toFile:userAccountPath)
+    }
+    
+    // MARK: - 解档
+    // 4.实现解档从Document文件夹获取本地模型数据
+    // 4.1 判断Document文件夹下是否有已保存好的模型,有的话就解档取出模型
+    func readWithNSKeyedUnarchiver(finished: @escaping (Content) ->()) -> () {
+        DispatchQueue.global().async { () -> Void in
+            if NSKeyedUnarchiver.unarchiveObject(withFile:self.userAccountPath) != nil {
+                
+                let userModel = (NSKeyedUnarchiver.unarchiveObject(withFile:self.userAccountPath) as? Content)!
+                finished(userModel)
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Swift-4.2/UserDefaultsViewController.swift
@@ -0,0 +1,112 @@
+//
+//  UserDefaultsViewController.swift
+//  Swift-4.2
+//
+//  Created by HL on 2018/8/17.
+//  Copyright © 2018年 XL. All rights reserved.
+//
+
+import UIKit
+
+class UserDefaultsViewController: UIViewController {
+    
+    var textFiled = UITextField()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "NSUserDefault"
+        
+        let saveItem = UIBarButtonItem(title: "save", style: .done, target: self, action: #selector(save))
+        let readItem = UIBarButtonItem(title: "read", style: .done, target: self, action: #selector(readInfo))
+        let deleteItem = UIBarButtonItem(title: "del", style: .done, target: self, action: #selector(deleteInfo))
+        
+        self.navigationItem.rightBarButtonItems = [saveItem, readItem, deleteItem]
+        
+        setupUI()
+        
+    }
+    
+    override func loadView() {
+        super.loadView()
+        self.view.backgroundColor = UIColor.white
+        if self.responds(to: #selector(getter: UIViewController.edgesForExtendedLayout)) {
+            self.edgesForExtendedLayout = UIRectEdge()
+        }
+    }
+
+    
+    // MARK: - 保存
+    func saveInfo(_ name : String){
+        if 0 <= name.count  {
+            let userDefault = UserDefaults.standard
+            userDefault.set(name, forKey: "name")
+            userDefault.synchronize()
+            
+            let alert = UIAlertController(title: nil, message: "保存成功", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "ok", style: .cancel, handler: nil)
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - 读取
+    @objc func readInfo() -> String {
+        let userDefault = UserDefaults.standard
+        let name = userDefault.object(forKey: "name") as? String
+        
+        let alert = UIAlertController(title: nil, message: "读取成功:\(String(describing: name))", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "ok", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+        
+        if (name != nil) {
+            return name!
+        }
+        return ""
+    }
+    
+    // MARK: - 删除
+    @objc func deleteInfo() {
+        let userDefault = UserDefaults.standard
+        userDefault.removeObject(forKey: "name")
+        
+        let alert = UIAlertController(title: nil, message: "删除成功", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "ok", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func setupUI(){
+        textFiled = UITextField(frame: CGRect(x: 10, y: 10, width: 200, height: 30))
+        self.view.addSubview(textFiled)
+        textFiled.clearButtonMode = .whileEditing
+        textFiled.returnKeyType = .done
+        textFiled.delegate = self as? UITextFieldDelegate
+        textFiled.textColor = UIColor.red
+        textFiled.layer.borderColor = UIColor.black.cgColor
+        textFiled.layer.borderWidth = 1.0
+    }
+    
+    @objc func save() {
+        self.saveInfo(textFiled.text!)
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
